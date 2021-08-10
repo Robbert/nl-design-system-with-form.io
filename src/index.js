2b7e972c6e65fe4410c241f2ca21ca8caf689a6a
Robbert Broersma <mail@robbertbroersma.nl>
2021-08-10 22:46:49 +0200

    templates

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import { Formio } from 'react-formio';
+import { Formio, Templates } from 'react-formio';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './config';
 import { AuthProvider } from './modules/auth';
-import formioComponents from './components/index'
+import { components, templates } from './components/index'
 
-Formio.use(formioComponents);
+Formio.use({ components });
+Templates.current = templates;
 
 ReactDOM.render(
   <React.StrictMode>
